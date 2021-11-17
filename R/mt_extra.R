ae28316f0b2e777660f09f3e6e22fe4eea2d4005
wanchanglin <wanchang_lin@outlook.com>
2021-11-17 18:48:41 +0000

    add range_scale

--- R/mt_extra.R
+++ R/mt_extra.R
@@ -1342,6 +1342,44 @@ std_scale <- function(x, na.rm = TRUE) {
 }
 
 ## ------------------------------------------------------------------------
+#' Map values to a defined range
+#' 
+#' Scale values to a defined range.
+#'
+#' @param x a numeric vector
+#' @param range a vector with two values: lower and higher.
+#' @param from.range a range values.  Default is NA.
+#' @return a scaled vector.
+#' @details. It is used mainly for controlling vertex size by a designed range
+#' @export 
+## wl-08-07-2015: Map values to a defined range. The previous name is 'map'.
+range_scale <- function(x, range = c(0, 1), from.range = NA) {
+  if (any(is.na(from.range))) from.range <- range(x, na.rm = TRUE)
+
+  ## check if all values are the same
+  if (!diff(from.range)) {
+    return(matrix(mean(range),
+      ncol = ncol(x), nrow = nrow(x),
+      dimnames = dimnames(x)
+    ))
+  }
+
+  ## map to [0,1]
+  x <- (x - from.range[1])
+  x <- x / diff(from.range)
+  ## handle single values
+  if (diff(from.range) == 0) x <- 0
+
+  ## map from [0,1] to [range]
+  if (range[1] > range[2]) x <- 1 - x
+  x <- x * (abs(diff(range))) + min(range)
+
+  x[x < min(range) | x > max(range)] <- NA
+
+  return(x)
+}
+
+## ------------------------------------------------------------------------
 #' Scale vector between min and max
 #' 
 #' Scale vector between mininum and maxinum values.
